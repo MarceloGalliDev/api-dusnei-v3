a39ca224d3bf7afec952eef899101547d6f236e7
Marcelo Galli <marcelolemesgalli@hotmail.com>
2023-06-28 22:58:47 -0300

    update
    update

--- api/services/departamentos_service.py
+++ api/services/departamentos_service.py
@@ -5,5 +5,5 @@ from api import db
 def listar_departamentos():
     dpto = aliased(departamentos_model.DepartamentosModel)
     departamentos = db.session.query(dpto.dpto_codigo, dpto.dpto_descricao).filter(
-        dpto.dpto_codigo.notin_ (['900', '500', '400', '300'])).all()
+        dpto.dpto_codigo.notin_ (['900', '500', '400', '300'])).order_by(dpto.dpto_codigo).all()
     return [{'dpto_codigo': d.dpto_codigo, 'dpto_descricao': d.dpto_descricao} for d in departamentos]
