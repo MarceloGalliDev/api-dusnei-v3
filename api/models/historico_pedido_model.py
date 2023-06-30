3e66743c6cff1d7fd97b79d211dedcf0204825a5
Marcelo Galli <marcelolemesgalli@hotmail.com>
2023-06-30 14:42:20 -0300

    update
    update

--- api/models/historico_pedido_model.py
+++ api/models/historico_pedido_model.py
@@ -1,19 +1,36 @@
 from api import db
 
-class CidadesModel(db.Model):
+class HistoricoPedidosCModel(db.Model):
     __tablename__ = 'movprodc'
     
-    mprc_tran_codigo = db.Column(db.String(5))#codigo do transportador, ligado a tabela transportadores
-    mprc_codentidade = db.Column(db.Numeric(precision=8, scale=0))#codigo do cliente
-    mprc_numerodcto = db.Column(db.String(20), nullable=False)#numero nfe
+    mprc_unid_codigo = db.Column(db.String(3), nullable=False)
+    mprc_codentidade = db.Column(db.Numeric(precision=8, scale=0))
+    mprc_numerodcto = db.Column(db.String(20), nullable=False)
+    mprc_peso = db.Column(db.Numeric(precision=12, scale=3))
+    mprc_tran_codigo = db.Column(db.String(5))
+    mprc_dcto_codigo = db.Column(db.String(4))
+    mprc_status= db.Column(db.String(1))
+    mprc_prvdapadrao = db.Column(db.Numeric(precision=12, scale=2))
+    mprc_prvenda = db.Column(db.Numeric(precision=12, scale=2))
+    mprc_fpgt_codigo = db.Column(db.String(3))
+    mprc_datamvto = db.Column(db.Date)
     
-   
     
     def to_dict(self):
         return {
-            'transportador': self.mprc_tran_codigo,
+            'codemitente': self.mprc_unid_codigo,
             'codcli': self.mprc_codentidade,
             'numnota': self.mprc_numerodcto,
+            'totpeso': self.mprc_peso,
+            'origemped': self.mprc_dcto_codigo,
+            'posicao': self.mprc_status,
+            'vltabela': self.mprc_prvdapadrao,
+            'condvenda': self.mprc_dcto_codigo,
+            'codfilial': self.mprc_unid_codigo,
+            'vlatend': self.mprc_prvenda,
+            'codcob': self.mprc_fpgt_codigo,
+            'vltotal': self.mprc_prvenda,
+            'data': self.mprc_datamvto,
             
-
+            'transportador': self.mprc_tran_codigo,
         }
