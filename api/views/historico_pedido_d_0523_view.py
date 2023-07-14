a2055b87bd1c9ab07c05103e1fa0ac2513a3552a
Marcelo Galli <marcelolemesgalli@hotmail.com>
2023-07-14 09:27:07 -0300

    update
    update

--- api/views/historico_pedido_d_0523_view.py
+++ api/views/historico_pedido_d_0523_view.py
@@ -38,7 +38,7 @@ class HistoricoPedidosD0523List(Resource):
             datefmt='%d/%m/%Y %I:%M:%S %p -'
         )
         
-        self.post(historico_pedido_d_items)
+        # self.post(historico_pedido_d_items)
         
         response = make_response(json.dumps(
             historico_pedido_d_items,
@@ -55,47 +55,47 @@ class HistoricoPedidosD0523List(Resource):
         response.mimetype = 'application/json'
         return response
 
-    def post(self, data):
-        url = os.getenv('URL_HISTORICOS_PEDIDOS_ITENS')
-        token = os.getenv('TOKEN')
-        headers = {
-            'Content-Type': 'application/json',
-            'Authorization': f'Bearer {token}'
-        }
+    # def post(self, data):
+    #     url = os.getenv('URL_HISTORICOS_PEDIDOS_ITENS')
+    #     token = os.getenv('TOKEN')
+    #     headers = {
+    #         'Content-Type': 'application/json',
+    #         'Authorization': f'Bearer {token}'
+    #     }
         
-        dados_iterados = len(data) // 5000
-        if len(data) % 5000:
-            dados_iterados += 1
+    #     dados_iterados = len(data) // 5000
+    #     if len(data) % 5000:
+    #         dados_iterados += 1
             
-        for i in range(dados_iterados):
-            try:
-                start = i * 5000
-                end = start + 5000
-                chunk = data[start:end]
+    #     for i in range(dados_iterados):
+    #         try:
+    #             start = i * 5000
+    #             end = start + 5000
+    #             chunk = data[start:end]
                 
-                response = requests.post(
-                    url, 
-                    headers=headers, 
-                    data=json.dumps(
-                        chunk,
-                        ensure_ascii=False, 
-                        use_decimal=True, 
-                        indent=4, 
-                        default=lambda o: o.isoformat() if isinstance(
-                            o, 
-                            (datetime.date, datetime.datetime)
-                        ) else None)
-                    )
-                response.raise_for_status()
+    #             response = requests.post(
+    #                 url, 
+    #                 headers=headers, 
+    #                 data=json.dumps(
+    #                     chunk,
+    #                     ensure_ascii=False, 
+    #                     use_decimal=True, 
+    #                     indent=4, 
+    #                     default=lambda o: o.isoformat() if isinstance(
+    #                         o, 
+    #                         (datetime.date, datetime.datetime)
+    #                     ) else None)
+    #                 )
+    #             response.raise_for_status()
                 
-                logging.info(f'Dados {i+1} enviado com sucesso!')
+    #             logging.info(f'Dados {i+1} enviado com sucesso!')
                 
-                if i != dados_iterados - 1:
-                    time.sleep(20)
-            except requests.exceptions.HTTPError as err:
-                logging.error(f'HTTP Error: {err}')
-            except Exception as err:
-                logging.error(f'API Error: {err}')
-                logging.error(f'Full Error: {traceback.format_exc()}')
+    #             if i != dados_iterados - 1:
+    #                 time.sleep(20)
+    #         except requests.exceptions.HTTPError as err:
+    #             logging.error(f'HTTP Error: {err}')
+    #         except Exception as err:
+    #             logging.error(f'API Error: {err}')
+    #             logging.error(f'Full Error: {traceback.format_exc()}')
 
 api.add_resource(HistoricoPedidosD0523List, '/historico-pedido-d-0523') 
